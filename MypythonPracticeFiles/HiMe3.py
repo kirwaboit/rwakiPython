3c7f959c9cb9a8749f31142b1b38f47d3d9a4c5a
janevieve state <janevieveaws@gmail.com>
2021-05-02 11:23:20 -0500

    cleaned up classes code

--- MypythonPracticeFiles/HiMe3.py
+++ MypythonPracticeFiles/HiMe3.py
@@ -1,24 +1,10 @@
+# In this example we instead use a JSON object that has already been created for us
 
-''' CODE FOR HOMEWORK 3.3'''
-score = input("Enter Score: ")
+import json
 
-try:
-    score = float(score)
-except:
-    print("error")
-    exit()
+with open(r'C:\Users\Burudani\Documents\mainPythonFolder_v1\JSON\dictData2.json') as json_file:
+    data = json.loads(json_file.read()) # this parses the JSON string
 
-
-if ((score<0.0) or (score>1.0)):
-    print("out of range")
-    exit()
-elif score>=0.9:
-    print ("A")
-elif score >= 0.8:
-    print("B")
-elif score >= 0.7:
-    print("C")
-elif score >= 0.6:
-    print("D")
-elif score < 0.6:
-    print("F")
+    print(data)
+    print('data type is:-')
+    print(type(data))
