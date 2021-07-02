377833ecfcf79140b3761ca6f4cb770e56232d44
janevieve state <janevieveaws@gmail.com>
2021-07-02 13:25:50 -0500

    Burudani :testing modified username

--- rkp.sh
+++ rkp.sh
@@ -3,5 +3,5 @@
 echo Starting Push for user :  $(whoami), Please enter Commit Message:
 read varMessage
 git add .
-git commit -m "$varMessage"
+git commit -m "$(whoami) :$varMessage"
 git push "https://gitlab-ci-token:TqCszEqvPgUyWoA-UZtC@gitlab.com/rwaki/rwakipythonpractice.git" master
