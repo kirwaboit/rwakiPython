ffef168b74e0f5162820890e59dc042b65d83a19
janevieve state <janevieveaws@gmail.com>
2021-07-06 06:12:00 -0500

    Burudani: preparing for pull to laptop

--- rk_push.sh
+++ rk_push.sh
@@ -4,4 +4,4 @@ echo Starting Push for user :  $(whoami), Please enter Commit Message:
 read varMessage  # this prompts the user for an input messsage , then saves the result in a variable
 git add .
 git commit -m "$(whoami): $varMessage"
-git push "https://gitlab-ci-token:TqCszEqvPgUyWoA-UZtC@gitlab.com/rwaki/rwakipythonpractice.git" master
+git push "https://gitlab-ci-token:4Qn66sT5J4Ti2vGJzRbY@gitlab.com/rwaki/rwakipythonpractice.git" master
