152648ff8721648f4ac0d8cab9d8235d09644077
janevieve state <janevieveaws@gmail.com>
2021-07-07 22:05:35 -0500

    Burudani: adding batch files

---
+++ Batch_Files/firstBatchfile.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd "C:\Users\Burudani\OneDrive\Documents\OnedriveTestFolder\FolderforGitClone"
+git clone "https://gitlab-ci-token:gjsNyxgD4aMhnjL_iAM_@gitlab.com/rwaki/rwakipythonpractice.git"
+ECHO Congratulations! Your first batch file was executed successfully.
