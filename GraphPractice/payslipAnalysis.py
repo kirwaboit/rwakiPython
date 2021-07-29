fba989e4c4a3fa8279f1e3e8995093344a42678b
janevieve state <janevieveaws@gmail.com>
2021-07-28 20:43:14 -0500

    Burudani: adding arduino code

--- GraphPractice/payslipAnalysis.py
+++ GraphPractice/payslipAnalysis.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 import matplotlib.dates as mdates
+import matplotlib
 #import datetime
 
-plt.style.use('ggplot')
+#plt.style.use('ggplot')
 
 date_cols = ['Date']
 df1 = pd.read_excel(r"C:\Users\Burudani\Documents\Finance_Trending\Checking_Jan1st2021_March10th2021.xlsx",
@@ -13,9 +14,14 @@ parse_dates = date_cols, # you can pass in a single value or a list of values
 index_col = 'Date',
 na_values= None) # specify how to deal with blank values
 
-
-fig, ax = plt.subplots()
-ax.bar(df1.index,df1['Amount'], color = "b",label="Money") # using bar instead of plot this time
+#locs, labels = xticks()  # Get the current locations and labels.
+#plt.xticks([0, 1, 2], ['January', 'February', 'March'])
+#df1.set_index('Date', inplace=True)
+df1.resample('1M').sum()['Amount'].plot()
+#df1.resample('1M').count()['Amount'].plot(kind='bar')
+#df.groupby('ticker')['adj_close'].plot(legend=True)
+#fig, ax = plt.subplots()
+#ax.bar(df1.index,df1['Amount'], color = "b",label="Money") # using bar instead of plot this time
 
 #df1 = df1.groupby(df1['Date'].dt.month,as_index=True,sort=False)['Amount'].sum()
 #df2 = df2.fillna('')
@@ -37,4 +43,15 @@ ax.bar(df1.index,df1['Amount'], color = "b",label="Money") # using bar instead o
 #plt.show()
 #df1.plot()
 #ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+#plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+#plt.grid(axis = 'y',linestyle = '--')
+#plt.minorticks_on()
+#plt.majorticks_on()
+#plt.tick_params(which='major', linestyle = ':')
+
+# plt.grid(which='major')
+
+plt.grid(which='both')
+plt.grid(which='minor', linestyle = ':')
+
 plt.show()
