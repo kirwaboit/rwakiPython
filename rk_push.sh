976ade1eb353eca66875cc20f64813cb327a0db2
rwaki <instrumetric.net@gmail.com>
2022-02-01 04:55:37 -0600

    ![cat scratch](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)

--- rk_push.sh
+++ rk_push.sh
@@ -3,7 +3,8 @@
 echo Starting Push for user :  $(whoami), Please enter Commit Message below:
 read varMessage  # this prompts the user for an input messsage , then saves the result in a variable
 git add .
-git commit -m "$(whoami): $varMessage"
+#git commit -m "$(whoami): $varMessage"
+git commit -m  "$varMessage"
 git push "https://gitlab-ci-token:gjsNyxgD4aMhnjL_iAM_@gitlab.com/rwaki/rwakipythonpractice.git" master
 
 
