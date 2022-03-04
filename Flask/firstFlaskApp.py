e1601d1d0e438c5c42591e06142934811ed07e29
rwaki <instrumetric.net@gmail.com>
2022-03-03 19:22:55 -0600

    rwaki: latest push before going to Janevieves place

--- Flask/firstFlaskApp.py
+++ Flask/firstFlaskApp.py
@@ -2,7 +2,7 @@ from flask import Flask
   
 app = Flask(__name__) #creating the Flask class object   
  
-@app.route('/') #decorator drfines the   
+@app.route('/') #decorator defines the   
 def home():  
     return "hello, this is our first flask website";  
   
---
+++ PowerBi/README.md
@@ -0,0 +1,4 @@
+# Term Definitions
+
+- Qmix = private medicaid and others
+- 9864 /921   10.71
