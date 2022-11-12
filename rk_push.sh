b83f9ce971affd6624ff66fee9ea5770ef5b5cf6
rwaki <instrumetric.net@gmail.com>
2022-11-12 13:48:54 -0600

    Burudani: My first commit to github, hope it all goes well

--- rk_push.sh
+++ rk_push.sh
@@ -5,7 +5,10 @@ read varMessage  # this prompts the user for an input messsage , then saves the 
 git add .
 git commit -m "$(whoami): $varMessage"  # this is an alternative way to show person who commited message
 #git commit -m  "$varMessage"    # this is an alternative without showing person who commited message  
-git push "https://gitlab-ci-token:gjsNyxgD4aMhnjL_iAM_@gitlab.com/rwaki/rwakipythonpractice.git" master
+
+
+#git push "https://gitlab-ci-token:gjsNyxgD4aMhnjL_iAM_@gitlab.com/rwaki/rwakipythonpractice.git" master
+git push "https://github.com/kirwaboit/rwakiPython.git" master
 
 
 
